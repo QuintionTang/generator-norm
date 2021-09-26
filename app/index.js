c762d6e331f38e6c4d4054bf82afcbdf42edebd6
QuintionTang <QuintionTang@gmail.com>
2021-09-26 19:02:18 +0800

    fix:调试工具结构

--- app/index.js
+++ app/index.js
@@ -1,224 +1,115 @@
-"use strict";
-var util = require("util");
-var path = require("path");
-var yeoman = require("yeoman-generator");
-var spawn = require("child_process").spawn;
-var chalk = require("chalk");
+'use strict';
+const Generator = require('yeoman-generator');
+const commandExists = require('command-exists').sync;
+const yosay = require('yosay');
+const mkdirp = require('mkdirp');
+const config = require('./config');
 
-var NormGenerator = (module.exports = function Generator(args, options) {
-    yeoman.generators.Base.apply(this, arguments);
-    this.indexFile = this.readFileAsString(
-        path.join(this.sourceRoot(), "index.shtml")
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    for (let optionName in config.options) {
+      this.option(optionName, config.options[optionName]);
+    }
+  }
+
+  initializing() {
+    this.pkg = require('../package.json');
+
+    if (this.options['skip-test-framework']) {
+      return;
+    }
+
+    this.composeWith(
+      require.resolve(
+        `generator-${this.options['test-framework']}/generators/app`
+      ),
+      {
+        'skip-install': this.options['skip-install']
+      }
     );
-    this.on("error", function () {});
-    this.on("end", function () {
-        this.installDependencies({
-            skipInstall: this.options["skip-install"],
-        });
+  }
+
+  prompting() {
+    if (!this.options['skip-welcome-message']) {
+      this.log(
+        yosay(
+          "'Allo 'allo! Out of the box I include HTML5 Boilerplate, jQuery, and a gulpfile to build your app."
+        )
+      );
+    }
+
+    return this.prompt(config.prompts).then(answers => {
+      const features = answers.features;
+      const hasFeature = feat => features && features.includes(feat);
+
+      // manually deal with the response, get back and store the results.
+      // we change a bit this way of doing to automatically do this in the self.prompt() method.
+      this.includeSass = hasFeature('includeSass');
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeModernizr = hasFeature('includeModernizr');
+      this.includeAnalytics = hasFeature('includeAnalytics');
+      this.includeJQuery = answers.includeJQuery;
     });
-    this.pkg = JSON.parse(
-        this.readFileAsString(path.join(__dirname, "../package.json"))
-    );
-});
+  }
 
-util.inherits(NormGenerator, yeoman.generators.Base);
+  writing() {
+    const templateData = {
+      appname: this.appname,
+      date: new Date().toISOString().split('T')[0],
+      name: this.pkg.name,
+      version: this.pkg.version,
+      includeSass: this.includeSass,
+      includeBootstrap: this.includeBootstrap,
+      testFramework: this.options['test-framework'],
+      includeJQuery: this.includeJQuery,
+      includeModernizr: this.includeModernizr,
+      includeAnalytics: this.includeAnalytics
+    };
 
-NormGenerator.prototype.askFor = function askFor() {
-    var cb = this.async(),
-        folderName = path.basename(process.cwd());
+    const copy = (input, output) => {
+      this.fs.copy(this.templatePath(input), this.destinationPath(output));
+    };
 
-    function parseProName(name) {
-        return name.replace(/\b(\w)|(-\w)/g, function (m) {
-            return m.toUpperCase().replace("-", "");
-        });
+    const copyTpl = (input, output, data) => {
+      this.fs.copyTpl(
+        this.templatePath(input),
+        this.destinationPath(output),
+        data
+      );
+    };
+
+    // Render Files
+    config.filesToRender.forEach(file => {
+      copyTpl(file.input, file.output, templateData);
+    });
+
+    // Copy Files
+    config.filesToCopy.forEach(file => {
+      copy(file.input, file.output);
+    });
+
+    // Create extra directories
+    config.dirsToCreate.forEach(item => {
+      mkdirp(item);
+    });
+
+    if (this.includeModernizr) {
+      copy('modernizr.json', 'modernizr.json');
     }
-    if (!this.options["skip-welcome-message"]) {
-        console.log(this.yeoman);
-        console.log(chalk.magenta("开始您的项目开发之旅^_^"));
-    }
 
-    var prompts = [
-        {
-            name: "projectName",
-            message: "项目名称（英文名称）？",
-            default: folderName,
-            warning: " ",
-            validate: function (answer) {
-                if (answer === "") {
-                    return "您必须输入项目名称^_^";
-                } else {
-                    var _pattern = /[\u4e00-\u9fa5]/g;
-                    if (_pattern.test(answer)) {
-                        return "项目名称必须是英文^_^";
-                    }
-                }
-                return true;
-            },
-        },
-        {
-            name: "description",
-            message: "项目描述？",
-            default: "一个WEB项目。",
-            warning: "",
-        },
-        {
-            name: "version",
-            message: "Version:",
-            default: "0.0.1",
-            warning: "",
-        },
-        {
-            type: "checkbox",
-            name: "features",
-            message:
-                "请选择项目所需要的类库（ClientApi、RegisterApi、Proxy、Common）？",
-            choices: [
-                {
-                    name: "跨域通讯代理库？",
-                    value: "includeProxy",
-                    checked: true,
-                },
-                {
-                    name: "RPC接口注册库？",
-                    value: "includeRegisterApi",
-                    checked: true,
-                },
-                {
-                    name: "谷歌统计？",
-                    value: "includeAnalysis",
-                    checked: true,
-                },
-                {
-                    name: "公共功能库（定时器Timer、参数获取Query）？",
-                    value: "includeCommon",
-                    checked: false,
-                },
-                {
-                    name: "黄金岛客户端通讯接口库？",
-                    value: "includeClientApi",
-                    checked: false,
-                },
-            ],
-        },
-    ];
-    this.prompt(
-        prompts,
-        function (props) {
-            this.packageName = props.projectName;
-            // project-name
-            this.projectName = parseProName(this.packageName);
-            //ProjectName
-            this.author = "4a volcano";
-            this.description = props.description;
-            this.version = props.version;
-            this.email = "";
-            var features = props.features;
+    let cssFile = `main.${this.includeSass ? 'scss' : 'css'}`;
+    copyTpl(cssFile, `app/styles/${cssFile}`, templateData);
+  }
 
-            function hasFeature(feat) {
-                return features.indexOf(feat) !== -1;
-            }
-
-            this.includeProxy = hasFeature("includeProxy");
-            this.includeRegisterApi = hasFeature("includeRegisterApi");
-            this.includeCommon = hasFeature("includeCommon");
-            this.includeClientApi = hasFeature("includeClientApi");
-            if (this.bitbucket !== "") {
-                cb();
-            }
-        }.bind(this)
-    );
-};
-
-/**
- * 初始化package.json
- */
-NormGenerator.prototype.packageJSON = function packageJSON() {
-    this.template("_package.json", "package.json");
-};
-
-/**
- * 生成gitignore
- */
-NormGenerator.prototype.git = function git() {
-    //生成git过滤规则
-    this.copy("_gitignore", ".gitignore");
-};
-
-NormGenerator.prototype.bowerJSON = function bowerJSON() {
-    this.template("_bower.json", ".bower.json");
-};
-
-/**
- * 初始化项目首页
- */
-NormGenerator.prototype.writeIndex = function writeIndex() {
-    var contentText = ["<h1>Hello World!</h1>", "<p>从这里开始！</p>"];
-    this.indexFile = this.indexFile.replace(
-        "<%= packageName %>",
-        this.packageName
-    );
-    this.indexFile = this.indexFile.replace(
-        '<div id="main">',
-        '<div id="main">\n\t\t\t' + contentText.join("\n\t\t\t")
-    );
-};
-
-/**
- *  初始化项目WEB文件
- */
-NormGenerator.prototype.site = function site() {
-    this.copy("favicon.ico", "src/favicon.ico");
-    this.copy("robots.txt", "src/robots.txt");
-    this.write("src/index.shtml", this.indexFile);
-};
-
-/**
- * 初始化scss
- */
-NormGenerator.prototype.scss = function scss() {
-    this.copy("_animate.sass", "scss/partials/_animate.sass");
-    this.copy("_common.scss", "scss/partials/_common.scss");
-    this.copy("master.scss", "scss/master.scss");
-};
-/**
- * 初始化scripts
- */
-NormGenerator.prototype.scripts = function scripts() {
-    this.copy("WebApp.js", "scripts/WebApp.js");
-    this.copy("websocket.js", "src/runner/websocket.js");
-};
-
-NormGenerator.prototype.gruntfile = function gruntfile() {
-    this.template("Gruntfile.js", "Gruntfile.js");
-};
-
-/**
- * 初始化项目目录
- */
-NormGenerator.prototype.app = function app() {
-    this.mkdir("src");
-    //项目核心文件夹，包括静态文件及页面文件
-    this.mkdir("src/assets");
-    this.mkdir("src/include");
-    //项目公共html文件
-    this.mkdir("src/assets/images");
-    //项目图片文件夹
-    this.mkdir("src/assets/styles");
-    //项目样式文件夹
-    this.mkdir("src/assets/scripts");
-    //项目编译后的JS脚本文件夹
-
-    this.mkdir("src/runner");
-    //测试服务器临时文件文件夹
-
-    this.mkdir("scripts"); //项目JS源码
-    this.mkdir("scss");
-    //项目SCSS文件夹
-    this.mkdir("scss/partials");
-    //公共SCSS文件
-
-    this.mkdir("doc");
-    //项目文档
-
-    this.template("README.md");
+  install() {
+    const hasYarn = commandExists('yarn');
+    this.installDependencies({
+      npm: !hasYarn,
+      yarn: hasYarn,
+      bower: false,
+      skipMessage: this.options['skip-install-message'],
+      skipInstall: this.options['skip-install']
+    });
+  }
 };
--- app/templates/README.md
+++
@@ -1,9 +0,0 @@
-# <%= packageName %>
-
-## 项目描述:
-
-<%= description %>
-
-### License
-
-Copyright (C) 2011 风林火山, All rights reserved.
